0499b2f54e98754d1a2510130acd7a8af5b23867
Admin <Admin@DESKTOP-0NJE2T0>
2017-02-09 10:53:51 +0100

    doin the commit ya

---
+++ PhotoSharingApplication/Controllers/FirstController.cs
@@ -0,0 +1,41 @@
+﻿using PhotoSharingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PhotoSharingApplication.Controllers
+{
+    public class FirstController : ApiController
+    {
+        PhotoSharingContext psc = new PhotoSharingContext();
+        [HttpGet]
+        public List<Photo> DoAdd()
+        {
+
+            return (psc.Photos.ToList());
+        }
+
+        [HttpPost]
+        public HttpStatusCode DeletePhoto(int id)
+        {
+            var selectedPhoto = psc.Photos.FirstOrDefault(x => x.PhotoID == id);
+            if (selectedPhoto != null)
+            {
+                psc.Photos.Remove(selectedPhoto);
+                psc.SaveChanges();
+                return HttpStatusCode.OK;
+            }
+            else return HttpStatusCode.NotFound;
+        }
+
+        public class Customer
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public bool Gay { get; set; }
+        }
+    }
+}
