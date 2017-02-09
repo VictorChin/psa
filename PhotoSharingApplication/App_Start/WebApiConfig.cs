252995321db151f157fe8acdb6277d5e9e7aacf7
Admin <Admin@DESKTOP-0NJE2T0>
2017-02-09 11:21:15 +0100

    local change

--- PhotoSharingApplication/App_Start/WebApiConfig.cs
+++ PhotoSharingApplication/App_Start/WebApiConfig.cs
@@ -22,6 +22,17 @@ namespace PhotoSharingApplication
                 routeTemplate: "api/photos/{title}",
                 defaults: new { controller = "PhotoApi", action = "GetPhotoByTitle" }
             );
+            config.Routes.MapHttpRoute(
+               name: "FirstApi",
+               routeTemplate: "api/first",
+               defaults: new { controller = "First", action = "GetAllPhotos", id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "FirstApiDelete",
+                routeTemplate: "api/first/{id}",
+                defaults: new { controller = "First", action = "DeletePhoto", id = RouteParameter.Optional }
+            );
 
             config.Routes.MapHttpRoute(
                 name: "PhotosApi",
---
+++ PhotoSharingApplication/Controllers/FirstController.cs
@@ -0,0 +1,38 @@
+﻿using PhotoSharingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PhotoSharingApplication.Controllers
+{
+    public class FirstController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Photo> GetAllPhotos()
+        {
+            PhotoSharingContext psc = new PhotoSharingContext();
+
+            return psc.Photos.ToList();
+        }
+        [HttpDelete]
+        public HttpStatusCode DeletePhoto(int id)
+        {
+            PhotoSharingContext psc = new PhotoSharingContext();
+            var photo =
+                psc.Photos.Where(p => p.PhotoID == id).FirstOrDefault();
+            if (photo == null) { return HttpStatusCode.NotFound; }
+            psc.Photos.Remove(photo);
+            psc.SaveChanges();
+            return HttpStatusCode.OK;
+        }
+
+    }
+    public class Customer
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
